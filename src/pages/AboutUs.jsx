c52b0ce67400d17d931a8ddb7eee187babacf980
Omar Soto <osjpsi@yandex.com>
2023-06-09 15:49:16 -0600

    pagina blog

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { Card, List, Pagination } from 'bootstrap';
 
-const AboutUs = () => {
+const Blog = () => {
+  const posts = [
+    {
+      titulo: 'Por que la pizza es tan rica?',
+      contenido: 'Aqui deberia de ir mucho texto, pero todos sabemos que la pizza es rica!',
+    },
+    {
+      titulo: 'Es el calzonene una quesadilla?',
+      contenido: 'Hoy descubriremos la verdad sobre este platillo',
+    },
+  ];
+
   return (
-    <div>AboutUs</div>
-  )
-}
+    <div>
+   <h1>Bienvenido a nuestro blog!</h1>
+      <List>
+        {posts.map((post) => (
+          <List.Item key={post.titulo}>
+            <Card>
+              <Card.Body>
+                <h4>{post.titulo}</h4>
+                <p>{post.contenido}</p>
+              </Card.Body>
+            </Card>
+          </List.Item>
+        ))}
+      </List>
 
+      <br />
+
+      <Pagination />
+    </div>
+  );
+};
 export default AboutUs
